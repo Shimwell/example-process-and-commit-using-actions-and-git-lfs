6b7be9dbc395f6305dd4ed782c67cb0a16a4f48a
Jonathan Shimwell <jonathan.shimwell@ukaea.uk>
2021-03-08 10:17:02 +0000

    adde initial files

---
+++ run_all.sh
@@ -0,0 +1,9 @@
+
+
+# download and install git-lfs is done on the 
+
+# git clone openmc nuclear data repo
+git clone https://github.com/openmc-dev/data.git
+
+# run script to generate h5 data
+python convert_nndc71.py
